136643be64cf00687b39f9ce920b605b33b97c48
ericpcho <ericpcho@gmail.com>
2017-08-31 11:18:57 -0700

    fully implemented bouncer

--- index.js
+++ index.js
@@ -1,58 +1,56 @@
-$.getJSON('http://localhost:8080/v2/listings/active')
+'use strict'
+const etsy_api = 'zw8s7fttg89x8cm4e4x88n39';
+const etsy_url = 'http://localhost:5000/v2/listings/active';
+const STORE = [];
+let input = []
 
-// 'use strict'
-// const etsy_api = 'zw8s7fttg89x8cm4e4x88n39';
-// const etsy_url = 'https://openapi.etsy.com/v2/listings/active';
-// const STORE = [];
-// let input = []
+function getDataFromApi(searchTerm, callback) {
+const findData = {
+    api_key: etsy_api,
+    q: searchTerm,
+}
 
-// function getDataFromApi(searchTerm, callback) {
-// const findData = {
-//     api_key: etsy_api,
-//     q: searchTerm,
-// }
+$.getJSON(etsy_url, findData, callback);
+}
 
-// $.getJSON(etsy_url, findData, callback);
-// }
+function watchSubmit() {
+  $('.search-form').submit(event => {
+    event.preventDefault();
+    console.log("Submit button working");
+    const queryTarget = $(event.currentTarget).find('.search-input');
+    const query = queryTarget.val();
+    input = query;
+    // clear out the input
+    queryTarget.val("");
+    getDataFromApi(query, displayEtsySearchData);
+    // $('.js-search-results').removeClass("hidden");
+  });
+}
 
-// function watchSubmit() {
-//   $('.search-form').submit(event => {
-//     event.preventDefault();
-//     console.log("Submit button working");
-//     const queryTarget = $(event.currentTarget).find('.search-input');
-//     const query = queryTarget.val();
-//     input = query;
-//     // clear out the input
-//     queryTarget.val("");
-//     getDataFromApi(query, displayEtsySearchData);
-//     // $('.js-search-results').removeClass("hidden");
-//   });
-// }
+function storeEtsySearchData(data) {
+  console.log(data);
+  // nextPageToken = data.nextPageToken
+  // prevPageToken = data.prevPageToken
+  data.items.map((item, index) => STORE.push(item));
+}
 
-// function storeEtsySearchData(data) {
-//   console.log(data);
-//   // nextPageToken = data.nextPageToken
-//   // prevPageToken = data.prevPageToken
-//   data.items.map((item, index) => STORE.push(item));
-// }
+function displayEtsySearchData() {
+  $('container').html(results);
+  const results = STORE.map(item => renderResult(item));
+}
 
-// function displayEtsySearchData() {
-//   $('container').html(results);
-//   const results = STORE.map(item => renderResult(item));
-// }
-
-// function renderResult(item) {
-//   return `
-//   <div class= "imgdiv">
+function renderResult(item) {
+  return `
+  <div class= "imgdiv">
       
-//   </div>
-//   `;
-// }
+  </div>
+  `;
+}
 
-// function handleEvents(){
-//   watchSubmit();
-//   displayEtsySearchData();
+function handleEvents(){
+  watchSubmit();
+  displayEtsySearchData();
 
-// }
+}
 
-// $(handleEvents);
+$(handleEvents);
