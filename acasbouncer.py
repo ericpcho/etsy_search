7d78b1cab525078aac380aa66ef6a54c2b0fbdc9
ericpcho <ericpcho@gmail.com>
2017-08-31 11:08:04 -0700

    installed bouncer

---
+++ acasbouncer.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+import requests
+app = Flask(__name__)
+
+@app.route("/v2/listings/active")
+def petfind():
+	r = requests.get("https://openapi.etsy.com" + request.full_path)
+	resp = jsonify(r.json())
+	resp.headers.add('Access-Control-Allow-Origin', '*')
+	return resp
+
+if __name__ == "__main__":
+	import logging
+	logging.basicConfig(level=logging.DEBUG)
+	app.run(host='0.0.0.0')
+
+  # https://openapi.etsy.com/v2/listings/active
+
--- index.js
+++ index.js
@@ -1,38 +1,44 @@
-const etsy_api = 'zw8s7fttg89x8cm4e4x88n39';
-const etsy_url = 'https://openapi.etsy.com/v2/listings/active';
-const STORE = [];
+$.getJSON('http://localhost:8080/v2/listings/active')
 
-function getDataFromApi(searchTerm, callback) {
-const findData = {
-    count:integer,
-    q: searchTerm,
-    key: etsy_api
-}
+// 'use strict'
+// const etsy_api = 'zw8s7fttg89x8cm4e4x88n39';
+// const etsy_url = 'https://openapi.etsy.com/v2/listings/active';
+// const STORE = [];
+// let input = []
 
-$.getJSON(etsy_url, findData, callback);
-}
+// function getDataFromApi(searchTerm, callback) {
+// const findData = {
+//     api_key: etsy_api,
+//     q: searchTerm,
+// }
 
-function watchSubmit() {
-  $(".search-button").submit(event => {
-    event.preventDefault();
-    console.log("Submit button working");
-    const queryTarget = $(event.currentTarget).find('.search-input');
-    const query = queryTarget.val();
-    // input = query;
-    // clear out the input
-    queryTarget.val("");
-    getDataFromApi(query, displayEtsySearchData);
-    // $('.js-search-results').removeClass("hidden");
-  });
-}
+// $.getJSON(etsy_url, findData, callback);
+// }
 
-// function displayEtsySearchData(data) {
+// function watchSubmit() {
+//   $('.search-form').submit(event => {
+//     event.preventDefault();
+//     console.log("Submit button working");
+//     const queryTarget = $(event.currentTarget).find('.search-input');
+//     const query = queryTarget.val();
+//     input = query;
+//     // clear out the input
+//     queryTarget.val("");
+//     getDataFromApi(query, displayEtsySearchData);
+//     // $('.js-search-results').removeClass("hidden");
+//   });
+// }
+
+// function storeEtsySearchData(data) {
 //   console.log(data);
 //   // nextPageToken = data.nextPageToken
 //   // prevPageToken = data.prevPageToken
 //   data.items.map((item, index) => STORE.push(item));
-//   const results = STORE.map(item => renderResult(item));
+// }
+
+// function displayEtsySearchData() {
 //   $('container').html(results);
+//   const results = STORE.map(item => renderResult(item));
 // }
 
 // function renderResult(item) {
@@ -43,9 +49,10 @@ function watchSubmit() {
 //   `;
 // }
 
-function handleEvents(){
-  watchSubmit();
+// function handleEvents(){
+//   watchSubmit();
+//   displayEtsySearchData();
 
-}
+// }
 
-$(handleEvents);
+// $(handleEvents);
